297a077a0d67cc7e3704a68c2d7fcfbe17ea5775
Zugfritze <70323844+Zugfritze@users.noreply.github.com>
2024-07-02 21:02:28 +0800

    格式化

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,13 +4,13 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
-    {
-        rules: {
-            "@typescript-eslint/triple-slash-reference": "off",
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/no-namespace": "off",
-        },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/triple-slash-reference": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-namespace": "off",
     },
+  },
 );
