5f9a876b9edd37008010bea4f6fa937688d78954
Zugfritze <70323844+Zugfritze@users.noreply.github.com>
2024-07-03 16:06:06 +0800

    更深入的使用类型系统
    添加了KeysOfTypeOrSubtype类型工具,用于获取对象中指定类型或其子类型的键。
    添加了ExcludeKeysOfTypeOrSubtype类型工具,用于排除对象中指定类型或其子类型的键。
    在ITypeSafeAccessor接口中添加了getAccessor方法,用于获取指定的类型安全访问器。
    在TypeSafeAccessor抽象类中添加了getAccessor方法,实现了ITypeSafeAccessor接口中的getAccessor方法。
    在TypeSafeAccessorProxy类中添加了getAccessor方法,实现了ITypeSafeAccessor接口中的getAccessor方法。

--- src/Tools/TypeSafeAccessor.ts
+++ src/Tools/TypeSafeAccessor.ts
@@ -1,9 +1,17 @@
-import { KeysOfType } from "./Types";
+import { ExcludeKeysOfTypeOrSubtype, KeysOfType } from "./Types";
 
 export interface ITypeSafeAccessor<T> {
   get<K extends keyof T>(key: K): T[K];
 
   get<V>(key: Extract<keyof T, KeysOfType<T, V>>): V;
+
+  getAccessor<K extends ExcludeKeysOfTypeOrSubtype<T, ITypeSafeAccessor<T[K]>>>(
+    key: K,
+  ): ITypeSafeAccessor<T[K]>;
+
+  getAccessor<V extends T[ExcludeKeysOfTypeOrSubtype<T, ITypeSafeAccessor<V>>]>(
+    key: Extract<keyof T, KeysOfType<T, V>>,
+  ): ITypeSafeAccessor<V>;
 
   set<K extends keyof T>(key: K, value: T[K]): void;
 
@@ -17,6 +25,18 @@ export abstract class TypeSafeAccessor
   get<V>(key: Extract<keyof this, KeysOfType<this, V>>): V;
   get<K extends keyof this>(key: K): this[K] {
     return this[key];
+  }
+
+  getAccessor<
+    K extends ExcludeKeysOfTypeOrSubtype<this, ITypeSafeAccessor<this[K]>>,
+  >(key: K): ITypeSafeAccessor<this[K]>;
+  getAccessor<
+    V extends this[ExcludeKeysOfTypeOrSubtype<this, ITypeSafeAccessor<V>>],
+  >(key: Extract<keyof this, KeysOfType<this, V>>): ITypeSafeAccessor<V>;
+  getAccessor<
+    K extends ExcludeKeysOfTypeOrSubtype<this, ITypeSafeAccessor<this[K]>>,
+  >(key: K): ITypeSafeAccessor<this[K]> {
+    return new TypeSafeAccessorProxy(this[key]);
   }
 
   set<K extends keyof this>(key: K, value: this[K]): void;
@@ -37,6 +57,18 @@ export class TypeSafeAccessorProxy<T> implements ITypeSafeAccessor<T> {
   get<V>(key: Extract<keyof T, KeysOfType<T, V>>): V;
   get<K extends keyof T>(key: K): T[K] {
     return this.object[key];
+  }
+
+  getAccessor<K extends ExcludeKeysOfTypeOrSubtype<T, ITypeSafeAccessor<T[K]>>>(
+    key: K,
+  ): ITypeSafeAccessor<T[K]>;
+  getAccessor<V extends T[ExcludeKeysOfTypeOrSubtype<T, ITypeSafeAccessor<V>>]>(
+    key: Extract<keyof T, KeysOfType<T, V>>,
+  ): ITypeSafeAccessor<V>;
+  getAccessor<K extends ExcludeKeysOfTypeOrSubtype<T, ITypeSafeAccessor<T[K]>>>(
+    key: K,
+  ): ITypeSafeAccessor<T[K]> {
+    return new TypeSafeAccessorProxy(this.object[key]);
   }
 
   set<K extends keyof T>(key: K, value: T[K]): void;
--- src/Tools/Types.ts
+++ src/Tools/Types.ts
@@ -18,3 +18,23 @@ export class ReferenceType<T extends PrimitiveType> {
 export type KeysOfType<T, V> = {
   [K in keyof T]: T[K] extends V ? K : never;
 }[keyof T];
+
+export type KeysOfTypeOrSubtype<T, I> = {
+  [K in keyof T]: T[K] extends I
+    ? K
+    : T[K] extends infer U
+      ? U extends I
+        ? K
+        : never
+      : never;
+}[keyof T];
+
+export type ExcludeKeysOfTypeOrSubtype<T, I> = {
+  [K in keyof T]: T[K] extends I
+    ? never
+    : T[K] extends infer U
+      ? U extends I
+        ? never
+        : K
+      : K;
+}[keyof T];
