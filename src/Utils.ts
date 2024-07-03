2d780b3a067256b7f08151c6537437eb989afd0f
Zugfritze <70323844+Zugfritze@users.noreply.github.com>
2024-07-03 11:36:28 +0800

    修改 TypeSafeAccessor 为抽象类，并添加接口 ITypeSafeAccessor 和代理类 TypeSafeAccessorProxy

--- src/Utils.ts
+++ src/Utils.ts
@@ -150,38 +150,48 @@ export type KeysOfType<T, V> = {
   [K in keyof T]: T[K] extends V ? K : never;
 }[keyof T];
 
-export class TypeSafeAccessor {
-  static get<T, K extends keyof T>(object: T, key: K): T[K];
+export interface ITypeSafeAccessor<T> {
+  get<K extends keyof T>(key: K): T[K];
 
-  static get<T, V>(object: T, key: Extract<keyof T, KeysOfType<T, V>>): V;
+  get<V>(key: Extract<keyof T, KeysOfType<T, V>>): V;
 
-  static get<T, K extends keyof T>(object: T, key: K): T[K] {
-    return object[key];
-  }
+  set<K extends keyof T>(key: K, value: T[K]): void;
 
-  static set<T, K extends keyof T>(object: T, key: K, value: T[K]): void;
-  static set<T, V>(
-    object: T,
-    key: Extract<keyof T, KeysOfType<T, V>>,
-    value: V,
-  ): void;
-  static set<T, K extends keyof T>(object: T, key: K, value: T[K]): void {
-    object[key] = value;
-  }
+  set<V>(key: Extract<keyof T, KeysOfType<T, V>>, value: V): void;
+}
 
+export abstract class TypeSafeAccessor
+  implements ITypeSafeAccessor<TypeSafeAccessor>
+{
   get<K extends keyof this>(key: K): this[K];
-
   get<V>(key: Extract<keyof this, KeysOfType<this, V>>): V;
-
   get<K extends keyof this>(key: K): this[K] {
     return this[key];
   }
 
   set<K extends keyof this>(key: K, value: this[K]): void;
-
   set<V>(key: Extract<keyof this, KeysOfType<this, V>>, value: V): void;
-
   set<K extends keyof this>(key: K, value: this[K]): void {
     this[key] = value;
+  }
+}
+
+export class TypeSafeAccessorProxy<T> implements ITypeSafeAccessor<T> {
+  private readonly object: T;
+
+  constructor(object: T) {
+    this.object = object;
+  }
+
+  get<K extends keyof T>(key: K): T[K];
+  get<V>(key: Extract<keyof T, KeysOfType<T, V>>): V;
+  get<K extends keyof T>(key: K): T[K] {
+    return this.object[key];
+  }
+
+  set<K extends keyof T>(key: K, value: T[K]): void;
+  set<V>(key: Extract<keyof T, KeysOfType<T, V>>, value: V): void;
+  set<K extends keyof T>(key: K, value: T[K]): void {
+    this.object[key] = value;
   }
 }
